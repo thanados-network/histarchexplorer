559bd0e7255ca39a112574a696afacc44f86d7d9
Bernhard Koschicek-Krombholz <b.koschicek@gmail.com>
2024-04-18 15:38:33 +0200

    added about

--- runserver.py
+++ runserver.py
@@ -4,9 +4,20 @@ app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_pyfile('production.py')
 
+
 @app.route('/')
 def index():
     return render_template('layout.html')
+
+
+@app.route('/sites')
+def sites():
+    return render_template('sites.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
