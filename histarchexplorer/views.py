92abbb96fbc21a33afef46067bcb82ca37933ca9
lisaldrian <lis.aldrian@gmail.com>
2024-04-23 15:20:58 +0200

    active link current page

--- histarchexplorer/views.py
+++ histarchexplorer/views.py
@@ -2,7 +2,6 @@ from flask import render_template
 
 from runserver import app
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -10,13 +9,13 @@ def index():
 
 @app.route('/entities')
 def entities():
-    return render_template('entities.html')
+    return render_template('entities.html', current_page='entities')
 
 @app.route('/search')
 def search():
-    return render_template('search.html')
+    return render_template('search.html', current_page='search')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', current_page='about')
