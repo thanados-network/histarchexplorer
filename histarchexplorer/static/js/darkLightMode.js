9abc7c1688a5740a9fff7cf011459f2c4f85ddfc
Stefan <stefan.eichert@univie.ac.at>
2024-04-24 16:26:58 +0200

    cleanup

--- histarchexplorer/static/js/darkLightMode.js
+++ histarchexplorer/static/js/darkLightMode.js
@@ -1,16 +1,8 @@
-function setTheme (mode = 'auto') {
+function setTheme(mode = 'auto') {
   const userMode = localStorage.getItem('bs-theme');
-  console.log('userMode')
-  console.log(userMode)
   const sysMode = window.matchMedia('(prefers-color-scheme: light)').matches;
-  console.log('sysMode')
-  console.log(sysMode)
   const useSystem = mode === 'system' || (!userMode && mode === 'auto');
-  console.log('useSystem')
-  console.log(useSystem)
-  const modeChosen = useSystem ? 'system' : mode === 'dark' || mode === 'light' ? mode : userMode;
-  console.log('modeChosen')
-  console.log(modeChosen)
+  const modeChosen = useSystem ? 'system' : ['dark', 'light'].includes(mode) ? mode : userMode;
 
   if (useSystem) {
     localStorage.removeItem('bs-theme');
@@ -18,14 +10,14 @@ function setTheme (mode = 'auto') {
     localStorage.setItem('bs-theme', modeChosen);
   }
 
-  document.documentElement.setAttribute('data-bs-theme', useSystem ? (sysMode ? 'light' : 'dark') : modeChosen);
-  let modeNotChosen = ''
-  if (modeChosen === 'dark') modeNotChosen = 'light'
-  if (modeChosen === 'light') modeNotChosen = 'dark'
+  const rootElement = document.documentElement;
+  rootElement.setAttribute('data-bs-theme', useSystem ? (sysMode ? 'light' : 'dark') : modeChosen);
+
+  const modeNotChosen = modeChosen === 'dark' ? 'light' : 'dark';
   document.getElementById(modeChosen).classList.add('d-none');
   document.getElementById(modeNotChosen).classList.remove('d-none');
-
 }
 
 setTheme();
+
 window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', () => setTheme());
