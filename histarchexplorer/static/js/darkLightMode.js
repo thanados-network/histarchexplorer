e8de82c1661afb0f68e02ad6eb28cd266967bb55
Stefan <stefan.eichert@univie.ac.at>
2024-04-25 12:09:36 +0200

    dark/light mode

--- histarchexplorer/static/js/darkLightMode.js
+++ histarchexplorer/static/js/darkLightMode.js
@@ -1,23 +1,47 @@
 function setTheme(mode = 'auto') {
-  const userMode = localStorage.getItem('bs-theme');
-  const sysMode = window.matchMedia('(prefers-color-scheme: light)').matches;
-  const useSystem = mode === 'system' || (!userMode && mode === 'auto');
-  const modeChosen = useSystem ? 'system' : ['dark', 'light'].includes(mode) ? mode : userMode;
+    let modeToUse;
+    const colorModes = ['light', 'dark']
+    const colorStored = colorModes.includes(localStorage.getItem('bs-theme'))
 
-  if (useSystem) {
-    localStorage.removeItem('bs-theme');
-  } else {
-    localStorage.setItem('bs-theme', modeChosen);
-  }
+    // Set modeToUse based on the provided mode parameter or localStorage value
+    if (colorModes.includes(mode)) {
+        modeToUse = mode;
+        localStorage.setItem('bs-theme', mode);
+    } else {
+        // If mode is not provided or invalid, determine mode based on user's system preference or localStorage
+        modeToUse = window.matchMedia('(prefers-color-scheme: dark)').matches && !colorStored
+            ? 'dark'
+            : colorStored
+                ? localStorage.getItem('bs-theme')
+                : 'light';
+    }
 
-  const rootElement = document.documentElement;
-  rootElement.setAttribute('data-bs-theme', useSystem ? (sysMode ? 'light' : 'dark') : modeChosen);
+    // Apply theme to the root element
+    const rootElement = document.documentElement;
+    rootElement.setAttribute('data-bs-theme', modeToUse);
 
-  const modeNotChosen = modeChosen === 'dark' ? 'light' : 'dark';
-  document.getElementById(modeChosen).classList.add('d-none');
-  document.getElementById(modeNotChosen).classList.remove('d-none');
+    // Hide the current theme's element and show the other one
+    const modeNotChosen = modeToUse === 'dark' ? 'light' : 'dark';
+    document.getElementById(modeToUse).classList.add('d-none');
+    document.getElementById(modeNotChosen).classList.remove('d-none');
+
+    const logo = document.getElementById('logo');
+    if (logo) {
+        const logoSrc = `/static/images/logos/logo_mode_${modeToUse}.svg`;
+        logo.setAttribute('src', logoSrc);
+    }
 }
 
+// Function to handle system mode change
+function systemModeChange() {
+    console.log('Theme change detected');
+    const mode = window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark';
+    setTheme(mode);
+}
+
+// Listen for changes in system color scheme and update the theme accordingly
+window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', systemModeChange);
+
+// Set initial theme based on localStorage or system preference
 setTheme();
 
-window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', () => setTheme());
