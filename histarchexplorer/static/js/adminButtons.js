c0980bf31dd4de712a605016c23045b35ae13fc7
lisaldrian <lis.aldrian@gmail.com>
2024-06-07 11:13:05 +0200

    map category - classes, array - show/hide via add remove d-none

--- histarchexplorer/static/js/adminButtons.js
+++ histarchexplorer/static/js/adminButtons.js
@@ -32,6 +32,21 @@ function addEntry(category){
     hiddenField.value = category.replace('nav-','')
     console.log(hiddenField.value)
 
+    const categoryClasses = {
+        'projects': ['projects-show'],
+        'institutions': ['institution-show'],
+        'persons': ['persons-show']
+    };
+
+    const allClasses = ['projects-show', 'institution-show', 'persons-show'];
+    allClasses.forEach(cls => {
+        document.querySelectorAll('.' + cls).forEach(el => el.classList.add('d-none'));
+    });
+
+    (categoryClasses[hiddenField.value] || []).forEach(cls => {
+        document.querySelectorAll('.' + cls).forEach(el => el.classList.remove('d-none'));
+    });
+
 
 const modal = new bootstrap.Modal(document.getElementById('addEntryModal'));
     modal.show();
