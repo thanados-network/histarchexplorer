a2baa2682f35435c3e6e902ba359d3dc1b24e7ed
lisaldrian <lis.aldrian@gmail.com>
2024-06-07 11:22:41 +0200

    descrip

--- histarchexplorer/static/js/adminButtons.js
+++ histarchexplorer/static/js/adminButtons.js
@@ -32,20 +32,21 @@ function addEntry(category){
     hiddenField.value = category.replace('nav-','')
     console.log(hiddenField.value)
 
+    //associate category with classes (map)
     const categoryClasses = {
         'projects': ['projects-show'],
         'institutions': ['institution-show'],
         'persons': ['persons-show']
     };
 
-    const allClasses = ['projects-show', 'institution-show', 'persons-show'];
+    const allClasses = ['projects-show', 'institution-show', 'persons-show']; //all classes that can be toggled
     allClasses.forEach(cls => {
         document.querySelectorAll('.' + cls).forEach(el => el.classList.add('d-none'));
-    });
+    }); // iterate over allclasses and add d-none to each element that has this class - hide all elements
 
     (categoryClasses[hiddenField.value] || []).forEach(cls => {
         document.querySelectorAll('.' + cls).forEach(el => el.classList.remove('d-none'));
-    });
+    }); //use hiddenField. value to get current category and retrieve the associated classes; remove d-none for each element that matches
 
 
 const modal = new bootstrap.Modal(document.getElementById('addEntryModal'));
