b05c8db4235e1fac77e7b459245c6e2c9e5dd1f1
lisaldrian <lis.aldrian@gmail.com>
2024-06-17 13:39:00 +0200

    setView map

--- histarchexplorer/static/js/map.js
+++ histarchexplorer/static/js/map.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     function initializeMap() {
-        // Replace '<your accessToken>' with your actual Jawg access token
         var accessToken = 'V2rWGMya8xJMEMpkjnXgkkPXM17NNEk3cNum1RvNUKMU6nspY9Bdi02PSyns93EA';
 
         // Initialize the map
         var map = L.map('map').setView([47.5162, 14.5501], 6);
 
-        // Define Jawg Light tile layer with access token
+        // Define Jawg Light tile layer
         var Jawg_Light = L.tileLayer('https://tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token=' + accessToken, {
             attribution: '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
             minZoom: 0,
@@ -16,7 +15,5 @@ document.addEventListener('DOMContentLoaded', function () {
         // Add Jawg Light tile layer to the map
         Jawg_Light.addTo(map);
     }
-
-    // Call initializeMap function when DOM content is loaded
     initializeMap();
 });
