eb6a20592acea3c5361670e76679dd9db3917073
Stefan <stefan.eichert@univie.ac.at>
2024-06-19 15:29:00 +0200

    about start

--- histarchexplorer/views/views.py
+++ histarchexplorer/views/views.py
@@ -22,20 +22,6 @@ def entities() -> str:
 def search() -> str:
     return render_template('search.html')
 
-
-@app.route('/about')
-def about() -> str:
-    sql = """
-        SELECT name, description FROM tng.config WHERE config_class = '1'
-    """
-
-    g.cursor.execute(sql)
-    result = g.cursor.fetchone()
-    project = result.name
-    description = result.description
-    return render_template('about.html', project=project, description=description)
-
-
 @app.route('/language=<language>')
 def set_language(language: Optional[str] = None) -> Response:
     session['language'] = language
