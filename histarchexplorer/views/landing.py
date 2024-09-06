e6349e3dd87520741d4344c52723e8855796df22
lisaldrian <lis.aldrian@gmail.com>
2024-09-06 16:01:05 +0200

    api contents trial

--- histarchexplorer/views/landing.py
+++ histarchexplorer/views/landing.py
@@ -1,11 +1,13 @@
 from flask import render_template, g
 
 from histarchexplorer import app
+from histarchexplorer.api.parser import Parser
+from histarchexplorer.models.entity import Entity
 
-from histarchexplorer.utils import cerberos
 
-
-
-@app.route('/entities/id')
-def landing() -> str:
-    return render_template('landing.html')
+@app.route('/entity/<id_>')
+def landing(id_: int) -> str:
+    parser = Parser()
+    entity = Entity.get_entity(id_, parser)
+    print(entity.types)
+    return render_template('landing.html', entity=entity)
