48aa3240f6b554904befc5d971e4d7e07a9f002e
cureboyxxx <eliasgruenbacher@gmail.com>
2025-06-05 09:55:27 +0200

    move sql statements from code to new file in database folder, repaired and reformatted about.html

---
+++ histarchexplorer/models/about.py
@@ -0,0 +1,51 @@
+from flask import g
+
+from histarchexplorer.database.about import build_connections_sql
+
+
+def build_object(build_connections, id):
+
+    g.cursor.execute('SELECT * FROM tng.config WHERE id = %s', (id,))
+    object_data = g.cursor.fetchone()
+    if not object_data:
+        return {}
+
+    object = {}
+    column_names = [description[0] for description in g.cursor.description]
+    for column_name, column_value in zip(column_names, object_data):
+        if column_value:
+            object[column_name] = column_value
+
+    # Integrate connections
+    object['connections'] = build_connections(object['id'])['properties']
+    return object
+
+
+def build_connections(id):
+    g.cursor.execute(build_connections_sql, (id,))
+    rows = g.cursor.fetchall()
+
+    result = {'properties': []}
+    property_map = {}
+
+    for row in rows:
+        property_id = row[1]
+        if property_id not in property_map:
+            property_obj = {'id': property_id, 'name': row[0], 'targets': []}
+            property_map[property_id] = property_obj
+            result['properties'].append(property_obj)
+
+    for row in rows:
+        property_id = row[1]
+        target_id = row[5]
+        target_obj = build_object(build_connections, target_id)
+        if target_obj and target_obj not in property_map[property_id]['targets']:
+            property_map[property_id]['targets'].append(target_obj)
+
+        role = row[3]
+        if role and 'roles' not in target_obj:
+            target_obj['roles'] = []
+        if role and role not in target_obj['roles']:
+            target_obj['roles'].append(role)
+
+    return result
--- histarchexplorer/views/about.py
+++ histarchexplorer/views/about.py
@@ -1,71 +1,21 @@
 import json
-from flask import render_template, g
+from flask import render_template
 from histarchexplorer import app
 from histarchexplorer.database.about import *
+from histarchexplorer.models.about import build_object, build_connections
 from histarchexplorer.utils import helpers
-
-
-
-
 
 
 @app.route('/about')
 def about() -> str:
 
-    def build_object(id_: int) -> None:
-        g.cursor.execute('SELECT * FROM tng.config WHERE id = %s', (id_,))
-    def build_object(id):
-
-        g.cursor.execute('SELECT * FROM tng.config WHERE id = %s', (id,))
-        object_data = g.cursor.fetchone()
-        if not object_data:
-            return {}
-
-        object = {}
-        column_names = [description[0] for description in g.cursor.description]
-        for column_name, column_value in zip(column_names, object_data):
-            if column_value:
-                object[column_name] = column_value
-
-        # Integrate connections
-        object['connections'] = build_connections(object['id'])['properties']
-        return object
-
-    def build_connections(id):
-        g.cursor.execute(build_connections_sql, (id,))
-        rows = g.cursor.fetchall()
-
-        result = {'properties': []}
-        property_map = {}
-
-        for row in rows:
-            property_id = row[1]
-            if property_id not in property_map:
-                property_obj = {'id': property_id, 'name': row[0], 'targets': []}
-                property_map[property_id] = property_obj
-                result['properties'].append(property_obj)
-
-        for row in rows:
-            property_id = row[1]
-            target_id = row[5]
-            target_obj = build_object(target_id)
-            if target_obj and target_obj not in property_map[property_id]['targets']:
-                property_map[property_id]['targets'].append(target_obj)
-
-            role = row[3]
-            if role and 'roles' not in target_obj:
-                target_obj['roles'] = []
-            if role and role not in target_obj['roles']:
-                target_obj['roles'].append(role)
-
-        return result
 
     # Example usage
-    print(json.dumps(build_object(1), ensure_ascii=False, indent=4))
+    #print(json.dumps(build_object(build_connections, 1), ensure_ascii=False, indent=4))
 
     g.cursor.execute('SELECT name, description, legal_notice, imprint FROM tng.config WHERE id = 1')
-    project_result = g.cursor.fetchone()
 
+    project_result = g.cursor.fetchone()
     project = {
         'name': (helpers.get_translation(project_result[0]))['label'],
         'description': (helpers.get_translation(project_result[1]))['label'],
