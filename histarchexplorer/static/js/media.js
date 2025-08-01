22f1627fe41725fe13acea8dd7a24d96b3107b55
lisaldrian <lis.aldrian@gmail.com>
2025-08-01 10:30:36 +0200

    display all media via macro

--- histarchexplorer/static/js/media.js
+++ histarchexplorer/static/js/media.js
@@ -17,4 +17,45 @@ window.mediaGrid = new Muuri('.grid-media', {
     }
 });
 
+// remove 3D Model spinner, when loaded
+document.querySelectorAll('model-viewer').forEach(model => {
+    model.addEventListener('poster-dismissed', () => {
+        const wrapper = model.closest('.model-wrapper');
+        if (wrapper) {
+            const spinner = wrapper.querySelector('.spinner');
+            if (spinner) spinner.style.display = 'none';
+        }
+        window.mediaGrid.refreshItems().layout();
+    });
+});
+
+// DOM loaded, initialize optional models
+document.addEventListener('DOMContentLoaded', () => {
+    if (customElements.get('model-viewer')) {
+        initModelViewers();
+        observeModelSizeChanges();
+    } else {
+        customElements.whenDefined('model-viewer').then(() => {
+            initModelViewers();
+            observeModelSizeChanges();
+        });
+    }
+
+    setTimeout(() => {
+        window.mediaGrid.refreshItems().layout();
+    }, 500);
+});
+
+
+function observeModelSizeChanges() {
+    const ro = new ResizeObserver(() => {
+        window.mediaGrid.refreshItems().layout();
+    });
+
+    document.querySelectorAll('model-viewer').forEach(model => {
+        ro.observe(model);
+    });
+}
+
+
 
