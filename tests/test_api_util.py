6d8eac225e7fa3c62ca16bf019b641433cb375c6
Bernhard Koschicek-Krombholz <b.koschicek@gmail.com>
2026-06-19 14:46:33 +0200

    fixed IIIF bug in tiles and added type division again

--- tests/test_api_util.py
+++ tests/test_api_util.py
@@ -119,3 +119,61 @@ def test_create_image_icon(app_instance):
             html = create_image_icon("default.svg")
             assert "static" in html
             assert "default.svg" in html
+
+
+def test_get_divisions_new_styles(app_instance):
+    with app_instance.test_request_context():
+        class MockSettings:
+            type_divisions = {
+                'Grave': {
+                    'ids': [10, 20],
+                    'icon_type': 'img',
+                    'icon_value': 'grave.svg'
+                },
+                'Building': {
+                    'ids': [30],
+                    'icon_type': 'css',
+                    'icon_value': 'bi-building'
+                }
+            }
+
+        g.settings = MockSettings()
+
+        # Test image division
+        res = get_divisions(10, [])
+        assert res['label'] == 'Grave'
+        assert 'grave.svg' in res['icon_url']
+        assert 'grave.svg' in res['iconUrl']
+
+        # Test css division
+        res = get_divisions(30, [])
+        assert res['label'] == 'Building'
+        assert 'bi-building' in res['icon']
+
+
+def test_get_divisions_fallback_logic(app_instance):
+    with app_instance.test_request_context():
+        if hasattr(g, 'settings'):
+            del g.settings
+
+        g.type_divisions = {
+            100: {
+                'label': 'Pit',
+                'icon': '<img src="/uploads/icons/pit.svg" />'
+            },
+            200: {
+                'label': 'Posthole',
+                'icon': '<i class="bi bi-circle"></i>'
+            }
+        }
+
+        # Test fallback image extraction
+        res = get_divisions(100, [])
+        assert res['label'] == 'Pit'
+        assert res['icon_url'] == '/uploads/icons/pit.svg'
+        assert res['iconUrl'] == '/uploads/icons/pit.svg'
+
+        # Test fallback css
+        res = get_divisions(200, [])
+        assert res['label'] == 'Posthole'
+        assert 'bi-circle' in res['icon']
